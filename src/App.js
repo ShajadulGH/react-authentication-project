69e1cd1ca1c25c7d098437f0ed5847c1d21c7738
Shajadul <shajadulrahe@gmail.com>
2023-01-04 18:51:51 +0600

    Feat: Protecting front-end pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        <Route path="/auth">
           <AuthPage />
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        <Route path="/profile">
+          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          {authCtx.isLoggedIn && <UserProfile />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
