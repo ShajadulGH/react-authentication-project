ebcd664f536b36a0f56387ca5d266a0aa12165a8
Shajadul <shajadulrahe@gmail.com>
2023-01-04 10:45:03 +0600

    Feat: Send sign up data to firebase

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -9,7 +9,35 @@ const AuthForm = () => {
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-  const submitHandler = (e) => {};
+  const submitHandler = (e) => {
+    console.log("working");
+
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const sendUser = async () => {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjqTuvP9UU9KIdm_58qGM0iISIAigCxsA",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+        }
+      );
+      const data = response.json();
+      console.log(data);
+    };
+    if (isLogin) {
+    } else {
+      sendUser();
+    }
+  };
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
